212679c114917e47bdd982ce9a366bb0008d8775
HEEEUN9812 <heeeun9812@gmail.com>
2024-01-02 01:54:14 +0900

    끝

--- src/Week_2/Collection/Col5.java
+++ src/Week_2/Collection/Col5.java
@@ -7,7 +7,7 @@ public class Col5 {
     public static void main(String[] args) {
         // 집합: 순서 없고, 중복 없음
         // 순서가 보장되지 않는 대신 중복을 허용하지 않도록 하는 프로그램에서 사용할 수 있는 자료 구조
-        // Set -> 그냥 쓸 수도 있음, HashSet, TreeSet 등ㅇ로 응용해서 같이 사용 가능
+        // Set -> 그냥 쓸 수도 있음, HashSet, TreeSet 등으로 응용해서 같이 사용 가능
         // Set은 생성자가 없는 껍데기라서 바로 생성할 수 없음
         // 생성자가 존재하는 HashSet을 이용해서 -> Set을 구현해 볼 수 있
 
--- src/Week_2/HW_2.java
+++ src/Week_2/HW_2.java
@@ -1,14 +1,67 @@
 package Week_2;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class HW_2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        case
-
+        String collectionType = sc.nextLine();
         String title = sc.nextLine();
-        String str = sc.nextLine();
+
+        switch (collectionType) {
+            case "List":
+                ArrayList<String> strList = new ArrayList<>();
+                while (true) {
+                    String content = sc.nextLine();
+                    if (Objects.equals(content, "끝")) {
+                        break;
+                    } else {
+                        strList.add(content);
+                    }
+                }
+                System.out.println("[ " + collectionType + " 로 저장된 " + title + "]");
+                for (int i = 0; i < strList.size(); i++) {
+                    System.out.println((i + 1) + ". " + strList.get(i));
+                }
+                break;
+
+            case "Set":
+                Set<String> strSet = new HashSet<>();
+                while (true) {
+                    String content = sc.nextLine();
+                    if (Objects.equals(content, "끝")) {
+                        break;
+                    } else {
+                        strSet.add(content);
+                    }
+                }
+                System.out.println("[ " + collectionType + " 으로 저장된 " + title + " ]");
+                Iterator<String> itrSet = strSet.iterator();
+                for (int i = strSet.size(); i < strSet.size(); i++) {
+                    System.out.println((i + 1) + ". " + itrSet.next());
+                }
+                break;
+
+            case "Map":
+                Map<Integer, String> strMap = new HashMap<>();
+                int number = 1;
+                while (true) {
+                    String content = sc.nextLine();
+                    if (Objects.equals(content, "끝")) {
+                        break;
+                    } else {
+                        strMap.put(number++, content);
+                    }
+                }
+                System.out.println("[ " + collectionType + " 으로 저장된 " + title + " ]");
+                for (int i = 0; i < strMap.size(); i++) {
+                    System.out.println((i+1) +". " + strMap.get(i + 1));
+                }
+                break;
+
+            default:
+                System.out.println("잘못 입력했습니다.");
+        }
     }
 }
